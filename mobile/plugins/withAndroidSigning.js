89304ade32708049627bbe9f62f4d59ba8fe89d6
alphan06visions-max <ismail10mafia@gmail.com>
2026-07-24 16:50:40 +0000

    Fix signing plugin: proper indent and target correct buildType blocks

--- mobile/plugins/withAndroidSigning.js
+++ mobile/plugins/withAndroidSigning.js
@@ -2,25 +2,29 @@ const { withAppBuildGradle } = require("@expo/config-plugins");
 
 const withAndroidSigning = (config) => {
   return withAppBuildGradle(config, (modConfig) => {
-    modConfig.modResults.contents = modConfig.modResults.contents.replace(
-      /buildTypes\s*\{/,
-      `signingConfigs {
-        release {
+    let contents = modConfig.modResults.contents;
+
+    // 1. Insert release block INSIDE the signingConfigs block, right after the debug block
+    // We find the closing } of the debug block inside signingConfigs and insert release after it
+    contents = contents.replace(
+      /(signingConfigs\s*\{[\s\S]*?debug\s*\{[\s\S]*?}\s*)(})/,
+      `$1    release {
             storeFile file("fridr.keystore")
             storePassword "fridr2026!"
             keyAlias "fridr"
             keyPassword "fridr2026!"
         }
-    }
-    buildTypes {`
+    $2`
     );
 
-    modConfig.modResults.contents = modConfig.modResults.contents.replace(
-      /(\brelease\s*\{)/,
-      `$q
-            signingConfig signingConfigs.release`
+    // 2. In the release buildType, change signingConfigs.debug to signingConfigs.release
+    // Target only the release block inside buildTypes
+    contents = contents.replace(
+      /(buildTypes\s*\{[\s\S]*?release\s*\{[\s\S]*?)signingConfig\s+signingConfigs\.debug/,
+      '$1signingConfig signingConfigs.release'
     );
 
+    modConfig.modResults.contents = contents;
     return modConfig;
   });
 };
